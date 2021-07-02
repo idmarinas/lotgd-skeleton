ef83d42c35298e2515d92e99250ee49341d6d67a
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-07-02 21:00:25 +0200

    Update for version 5.5.0

--- gulp/config/default.js
+++ gulp/config/default.js
@@ -26,13 +26,6 @@ const config = {
             //-- Include .files
             '{/**/.gitkeep,/.env,/.htaccess}',
             '!_core_files{,/**}', //-- Moved here for ignore .gitkeep files in this folder
-            //-- Ignore content of
-            '!data/installer{,/**}',
-            'data/installer/README.md', //-- But include README.md
-            //-- Ignore files of core
-            '!src/core{,/**}',
-            '!src/ajax/core{,/**}',
-            '!src/ajax/pattern/core{,/**}',
             //-- Ignore all dist folders/files
             '!{*.,}dist{,/**}',
             //-- Ignore dependency directories
