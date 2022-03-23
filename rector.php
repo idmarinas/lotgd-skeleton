1f1c4fd630abf7354f57d3acca07e79674d5f98c
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2022-03-23 13:21:03 +0100

    Updated to version 7.0.3

--- rector.php
+++ rector.php
@@ -6,6 +6,7 @@ require_once 'src/constants.php';
 
 use Rector\CodeQuality\Rector\Array_\CallableThisArrayToAnonymousFunctionRector;
 use Rector\CodeQuality\Rector\Include_\AbsolutizeRequireAndIncludePathRector;
+use Rector\CodeQuality\Rector\If_\ShortenElseIfRector;
 use Rector\Symfony\Set\SymfonySetList;
 use Rector\Core\Configuration\Option;
 use Rector\Core\ValueObject\PhpVersion;
@@ -25,6 +26,9 @@ return static function (ContainerConfigurator $containerConfigurator): void
         __DIR__ . '/tests',
     ]);
     $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_73);
+    $parameters->set(Option::AUTO_IMPORT_NAMES, true);
+    $parameters->set(Option::IMPORT_DOC_BLOCKS, false);
+    $parameters->set(Option::SYMFONY_CONTAINER_XML_PATH_PARAMETER, __DIR__ . '/var/cache/dev/App_KernelDevDebugContainer.xml');
 
     $containerConfigurator->import(SetList::DEAD_CODE);
     $containerConfigurator->import(SetList::CODE_QUALITY);
@@ -32,15 +36,28 @@ return static function (ContainerConfigurator $containerConfigurator): void
     $containerConfigurator->import(SetList::PHP_74);
     $containerConfigurator->import(SetList::PHP_80);
     $containerConfigurator->import(SetList::PHP_81);
+    $containerConfigurator->import(SetList::FRAMEWORK_EXTRA_BUNDLE_40);
+    $containerConfigurator->import(SetList::FRAMEWORK_EXTRA_BUNDLE_50);
 
     //-- Symfony Framework
+    $containerConfigurator->import(SymfonySetList::SYMFONY_40);
+    $containerConfigurator->import(SymfonySetList::SYMFONY_41);
+    $containerConfigurator->import(SymfonySetList::SYMFONY_42);
+    $containerConfigurator->import(SymfonySetList::SYMFONY_43);
     $containerConfigurator->import(SymfonySetList::SYMFONY_44);
     $containerConfigurator->import(TwigSetList::TWIG_240);
     $containerConfigurator->import(DoctrineSetList::DOCTRINE_25);
+    $containerConfigurator->import(DoctrineSetList::DOCTRINE_ORM_29);
+    $containerConfigurator->import(DoctrineSetList::DOCTRINE_CODE_QUALITY);
+    $containerConfigurator->import(DoctrineSetList::DOCTRINE_COMMON_20);
+    $containerConfigurator->import(DoctrineSetList::DOCTRINE_DBAL_210);
+    $containerConfigurator->import(DoctrineSetList::DOCTRINE_DBAL_211);
 
     //-- Skip some rules/files ...
     $parameters->set(Option::SKIP, [
         __DIR__.'/src/core/Twig/NodeVisitor',
+        ShortenElseIfRector::class,
+        EventListenerToEventSubscriberRector::class,
         CallableThisArrayToAnonymousFunctionRector::class,
         AbsolutizeRequireAndIncludePathRector::class
     ]);
---
+++ tests/TwigExtension/IntegrationTest.php
@@ -0,0 +1,87 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @see https://github.com/idmarinas/lotgd-game
+ *
+ * @license https://github.com/idmarinas/lotgd-game/blob/migration/public/LICENSE.txt
+ * @author IDMarinas
+ *
+ * @since 5.4.0
+ */
+
+namespace Lotgd\Bundle\Tests\TwigExtension;
+
+use Lotgd\Core\Http\Request;
+use Lotgd\Core\Navigation\AccessKeys;
+use Lotgd\Core\Navigation\Navigation as NavigationNavigation;
+use Lotgd\Core\Output\Format;
+use Lotgd\Core\Tool\Sanitize;
+use Lotgd\Core\Twig\Extension\ApplyFilter;
+use Lotgd\Core\Twig\Extension\CensorExtension;
+use Lotgd\Core\Twig\Extension\FormatExtension;
+use Lotgd\Core\Twig\Extension\GameCore;
+use Lotgd\Core\Twig\Extension\Jaxon;
+use Lotgd\Core\Twig\Extension\Navigation;
+use Lotgd\Core\Twig\Extension\SettingsExtension;
+use Lotgd\Core\Twig\Extension\Translator;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Twig\Test\IntegrationTestCase;
+
+/**
+ * Test Twig Extensions.
+ *
+ * @internal
+ */
+class IntegrationTest extends IntegrationTestCase
+{
+    public function getExtensions()
+    {
+        $client    = (new Kernel())->returnClient();
+        $container = $client->getContainer();
+
+        return [
+            new CensorExtension($container->get('lotgd_core.censor')),
+            new FormatExtension($container->get(Format::class)),
+            new GameCore(
+                $container->get(Request::class),
+                $container->get(Sanitize::class),
+                $container->get('translator'),
+                $container->get('lotgd_core.event_dispatcher'),
+                $container->get('lotgd_core.entity_manager'),
+                $container->get('session')
+            ),
+            new Navigation(
+                $container->get('translator'),
+                $container->get(NavigationNavigation::class),
+                $container->get(AccessKeys::class),
+                $container->get(Format::class),
+                $container->get(Request::class)
+            ),
+            new ApplyFilter(),
+            new Translator($container->get('translator')),
+            new Jaxon($container->get('lotgd.core.jaxon')),
+            new SettingsExtension($container->get('lotgd_core.settings'))
+        ];
+    }
+
+    public function getFixturesDir()
+    {
+        return __DIR__.'/Fixtures/';
+    }
+}
+
+/**
+ * @internal
+ * @coversNothing
+ */
+class Kernel extends WebTestCase
+{
+    public function returnClient()
+    {
+        self::ensureKernelShutdown();
+
+        return self::createClient();
+    }
+}
